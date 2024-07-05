fc92543250ff7f169ae475478b1a2182ea5c3d99
Adam Gorkiewicz <adamgoorkiewicz@gmail.com>
2024-07-05 19:52:11 +0300

    more fixes

--- drawings/text_decomposition.py
+++ drawings/text_decomposition.py
@@ -6,7 +6,6 @@ res = '\\documentclass{standalone}\n\\usepackage{tikz}\n\\begin{document}\n\\beg
 res += '\\usetikzlibrary{decorations.pathreplacing}'
 res += '\\usetikzlibrary{decorations.pathmorphing}'
 
-
 phi_x = 10
 phi_y = -3
 psi_x = 5
@@ -20,7 +19,6 @@ z = (n - 1) / 2
 
 random.seed(8)
 
-
 @dataclass
 class Point:
     x: float
@@ -32,12 +30,10 @@ class Point:
     def s(self):    return psi_x * self.y - psi_y * self.x
 
 
-
 S = [[0 for i in range(n)] for j in range(n)]
 T = [Point(i, j) for i in range(n) for j in range(n)]
 P = [Point(i, j) for i in range(m) for j in range(m)]
 Q = []
-
 
 def dot(u, v):      return u.x * v.x + u.y * v.y
 def cross(u, v):    return u.x * v.y - u.y * v.x
@@ -57,7 +53,6 @@ def max_x(points): return max(p.x for p in points)
 def min_y(points): return min(p.y for p in points)
 def max_y(points): return max(p.y for p in points)
 
-
 def draw_poly(vertices, settings = 'black'):
     global res
     vertices = [(v.x, v.y) if type(v) == Point else v for v in vertices]
@@ -66,16 +61,13 @@ def draw_poly(vertices, settings = 'black'):
     vertices = f'\\draw[{settings}] ' + vertices
     res += vertices + '\n'
 
-
-def fill_rect(u, v, settings = ''):
+def draw_rect(u, v, settings = ''):
     global res
-    res += f'\\fill [{settings}] ({min(u[0], v[0])}, {min(u[1], v[1])}) rectangle ({max(u[0], v[0])}, {max(u[1], v[1])});'
-
+    res += f'\\draw [{settings}] ({min(u[0], v[0])}, {min(u[1], v[1])}) rectangle ({max(u[0], v[0])}, {max(u[1], v[1])});'
 
 def draw_dots(s):
     global res
-    res += f'\\foreach \\x in {{0,...,{s - 1}}} \\foreach \\y in {{0,...,{s - 1}}} \\draw[opacity=0.8] (\\x, \\y) circle (0.5pt);'
-
+    res += f'\\foreach \\x in {{0,...,{s - 1}}} \\foreach \\y in {{0,...,{s - 1}}} \\draw[opacity=0.9] (\\x, \\y) circle (0.6pt);'
 
 for i in range(10):
     x = random.randint(1, n - m - 1)
@@ -84,8 +76,10 @@ for i in range(10):
     for u in P:
         S[x + u.x][y + u.y] = 1
 
+space = 5
+res +=  f'\\clip ({L - space}, {L - space}) rectangle ({R + space},{R + space});'
 
-fill_rect((L, L), (R, R), 'lightgray, opacity=0.7')
+draw_rect((L, L), (R, R), 'rounded corners=10, black, very thick, fill=lightgray, opacity=0.7')
 
 PA = []
 PB = []
@@ -111,27 +105,24 @@ for i in range(n - 1):
 P = PA[::-1] + PB + PD + PC[::-1]
 P = [Point(i + 0.5, j + 0.5) for (i, j) in P]
 
-draw_poly(P, 'rounded corners=10, black, very thick')
+draw_poly(P, 'rounded corners=10, black, very thick, fill=white')
 
 l = 40
-h_min = min([u.h for u in T])
-h_max = max([u.h for u in T])
-s_min = min([u.s for u in T])
-s_max = max([u.s for u in T])
 
+h_min = Point(L, L).h
+h_max = Point(R, R).h
+s_min = Point(R, L).s
+s_max = Point(L, R).s
+
+#h_min = min([u.h for u in T])
+#h_max = max([u.h for u in T])
+#s_min = min([u.s for u in T])
+#s_max = max([u.s for u in T])
 h_delta = (h_max - h_min) / l
 s_delta = (s_max - s_min) / l
 
 h = [h_min + i * h_delta for i in range(l + 1)]
 s = [s_min + i * s_delta for i in range(l + 1)]
-
-#h[0] -= 0.1 * h_delta
-#h[l] += 0.1 * h_delta
-#s[0] -= 0.1 * s_delta
-#s[l] += 0.1 * s_delta
-#for i in range(1, l):
-#    h[i] += random.uniform(-0.1 * h_delta, 0.1 * h_delta)
-#    s[i] += random.uniform(-0.1 * s_delta, 0.1 * s_delta)
 
 def coverable(points):
     ax = math.ceil(min_x(points))
@@ -158,7 +149,7 @@ def inside_text(points):
     #if 0 <= ax <= n     
     #return False
     for point in points:
-        if 0 <= point.x <= n and 0 <= point.y <= n:
+        if 0 <= point.x < n and 0 <= point.y < n:
             return True
     return False
 
@@ -181,7 +172,6 @@ for i in range(l):
     for j in range(l):
         if coverable(P[i][j]):
             C.append((i, j))
-
 
 for i in range(l):
     for j in range(l):
@@ -239,22 +229,19 @@ for i in range(l):
             a, b = min(t), max(t) + 1
             draw_poly([w[i][a], w[i][b], w[i + 1][b], w[i + 1][a]], 'very thick, internal border=red, fill=red, fill opacity=0.2')
 
-
-
-for h in h:
-    a = (x_hy(h, L), L) if x_hy(h, L) <= R else (R, y_hx(h, R))
-    b = (x_hy(h, R), R) if x_hy(h, R) >= L else (L, y_hx(h, L))
-    draw_poly([a, b], 'opacity=0.2')
-        
-
-for s in s:
-    a = (x_sy(s, L), L) if x_sy(s, L) >= L else (L, y_sx(s, L))
-    b = (x_sy(s, R), R) if x_sy(s, R) <= R else (R, y_sx(s, R))
-    draw_poly([a, b], 'opacity=0.2')
-
-
-#draw_poly([(z, L), (z, R)], 'opacity=0.4')
-#draw_poly([(L, z), (R, z)], 'opacity=0.4')
+#for h in h:
+#    #a = (x_hy(h, L), L) if x_hy(h, L) <= R else (R, y_hx(h, R))
+#    #b = (x_hy(h, R), R) if x_hy(h, R) >= L else (L, y_hx(h, L))
+#    a = xy(h, s_min)
+#    b = xy(h, s_max)
+#    draw_poly([a, b], 'opacity=0.2')
+#
+#for s in s:
+#    #a = (x_sy(s, L), L) if x_sy(s, L) >= L else (L, y_sx(s, L))
+#    #b = (x_sy(s, R), R) if x_sy(s, R) <= R else (R, y_sx(s, R))
+#    a = xy(h_min, s)
+#    b = xy(h_max, s)
+#    draw_poly([a, b], 'opacity=0.2')
 
 draw_dots(n)
 
